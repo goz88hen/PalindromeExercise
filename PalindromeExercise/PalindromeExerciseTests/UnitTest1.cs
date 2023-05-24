4f474bfbb721aab93fe5efc0bb91717eb57766ae
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-05-23 18:02:51 -0700

    Palindrome Exercise is complete

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,27 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("John", false)]
+        [InlineData("Jose", false)]
+        [InlineData("Richard", false)]
+        [InlineData("madam", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
 
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            // Arrange
+            var ws = new WordSmith();
+
+            //Act
+            var actual = ws.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmith.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (word == null || word.Length == 0)
+            {
+                return false;
+            }
+            string wordLowered = word.ToLower();
+            string backwards = "";
+
+                for (int i = wordLowered.Length - 1; i >= 0; i--)
+                {
+                    backwards += word[i];
+                }
+                if (word == backwards)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+           //return wordLowered == backwards; 
+        }
+    }
+}
