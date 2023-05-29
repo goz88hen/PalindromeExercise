1d43436f1cb21493df74269a16cf90695afd7afa
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-05-29 03:41:44 -0700

    Change UnitTest1 to WordSmith_Test.

---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmith_Test.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmith_Test
+    {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("John", false)]
+        [InlineData("Jose", false)]
+        [InlineData("Richard", false)]
+        [InlineData("madam", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            // Arrange
+            var ws = new WordSmith();
+
+            //Act
+            var actual = ws.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
